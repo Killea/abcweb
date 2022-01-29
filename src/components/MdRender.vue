086032d78d7c7bc42e597e9bf35b95d825085af1
Hank Wang <killea@gmail.com>
2022-01-29 00:00:22 -0500

    save md2

--- src/components/MdRender.vue
+++ src/components/MdRender.vue
@@ -1,10 +1,5 @@
 <template>
-  <div>
-    <object
-      data="./assets/javascript-entry-level.md"
-      width="300"
-      height="200"
-    ></object>
+  <div v-if="text">
     <vue-markdown>
       {{ text }}
     </vue-markdown>
@@ -16,30 +11,16 @@ export default {
   components: {
     VueMarkdown,
   },
-  created() {},
+  async mounted() {
+    const response = await fetch("/javascript-entry-level.md");
+    this.text = await response.text();
+    console.log(this.text)
+  },
+  computed:{ 
+  },
   data() {
     return {
-      text: `
-## Code
-Indented code
-
-    // Some comments
-    line 1 of code
-    line 2 of code
-    line 3 of code
-
-
-Block code "fences"
-
-## Tables
-
-| Option | Description |
-| ------ | ----------- |
-| data   | path to data files to supply the data that will be passed into templates. |
-| engine | engine to be used for processing templates. Handlebars is the default. |
-| ext    | extension to be used for dest files. |
-| 测试    | extension to be used for dest files. |
-`,
+      text: null
     };
   },
 };

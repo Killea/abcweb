3045d7e5638a5067a2a5b12bf534305720389570
Hank Wang <killea@gmail.com>
2022-01-28 23:44:44 -0500

    save md

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,171 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="menu-bar">
+      <vue-navigation-bar
+        :options="navbarOptions"
+        @vnb-item-clicked="menuClick"
+      />
+    </div>
+    <div class="body-item">
+      {{ currentPath }}
+      <template
+        v-if="currentPath==='/javascript-entry-level'">
+        <img alt="Vue logo" src="./assets/logo.png">
+        <JavaScriptEntryLevel msg="Welcome to Your Vue.js App" />
+      </template>
+    </div>
+
+    <!--  -->
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import JavaScriptEntryLevel from "./components/JavaScriptEntryLevel.vue"
+import Vue from "vue";
+import "vue-navigation-bar/dist/vue-navigation-bar.css";
+
+// import the library
+import VueNavigationBar from "vue-navigation-bar";
+Vue.component("vue-navigation-bar", VueNavigationBar);
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    JavaScriptEntryLevel,
+  },
+
+  data() {
+    return {
+      currentPath: "/",
+      navbarOptions: {
+        elementId: "main-navbar",
+        isUsingVueRouter: false,
+        mobileBreakpoint: 992,
+        brandImageAltText: "brand-image",
+        collapseButtonOpenColor: "#661c23",
+        collapseButtonCloseColor: "#661c23",
+        showBrandImageInMobilePopup: true,
+        ariaLabelMainNav: "Main Navigation",
+        tooltipAnimationType: "shift-away",
+        tooltipPlacement: "bottom",
+        menuOptionsLeft: [
+          {
+            type: "link",
+            text: "Coding lessons",
+            arrowColor: "#659CC8",
+            subMenuOptions: [
+              // {
+              //   isLinkAction: true,
+              //   type: "link",
+              //   text: "About",
+              //   subText: "Stupid corporate wet blankets. Like booze ever killed anyone.",
+              //   path: { name: "about" },
+              //   iconLeft: '<i class="fa fa-star fa-fw"></i>'
+              // },
+              // {
+              //   type: "hr",
+              // },
+              {
+                isLinkAction: true,
+                type: "link",
+                text: "JavaScript Entry Level",
+                subText: "Join now! Enrolling~",
+                path: { name: "locations" },
+                arrowColor: "#659CC8",
+              },
+              {
+                isLinkAction: true,
+                type: "link",
+                text: "JavaScript Mid Level",
+                subText: "Coming soon",
+                path: { name: "locations" },
+                arrowColor: "#659CC8",
+              },
+              // {
+              //   type: "hr",
+              // },
+              // {
+              //   type: "link",
+              //   text: "Blog",
+              //   subText: "I enjoy having breakfast in bed. I like waking up to the smell of bacon. Sue me.",
+              //   path: { name: "blog" }
+              // },
+            ],
+          },
+          {
+            isLinkAction: true,
+            type: "link",
+            text: "About us",
+            subMenuOptions: [
+              {
+                isLinkAction: true,
+                type: "link",
+                text: "Contact us",
+                path: { name: "customer-service" },
+              },
+              // {
+              //   type: "link",
+              //   text: "Accounting",
+              //   path: { name: "accounting" },
+              // },
+              // {
+              //   type: "hr",
+              // },
+              // {
+              //   type: "link",
+              //   text: "Reception",
+              //   path: { name: "reception"},
+              //   iconLeft: '<svg id="i-telephone" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="32" height="32" fill="none" stroke="currentcolor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"> <path d="M3 12 C3 5 10 5 16 5 22 5 29 5 29 12 29 20 22 11 22 11 L10 11 C10 11 3 20 3 12 Z M11 14 C11 14 6 19 6 28 L26 28 C26 19 21 14 21 14 L11 14 Z" /> <circle cx="16" cy="21" r="4" /> </svg>',
+              // },
+            ],
+          },
+          {
+            isLinkAction: true,
+            type: "link",
+            text: "Blog",
+            path: { name: "blog" },
+            iconRight: '<i class="fa fa-long-arrow-right fa-fw"></i>',
+          },
+        ],
+        // menuOptionsRight: [
+        //   {
+        //     type: "button",
+        //     text: "Signup",
+        //     path: { name: "signup" },
+        //     class: "button-red"
+        //   },
+        //   {
+        //     type: "button",
+        //     text: "Login",
+        //     path: { name: "login" },
+        //     iconRight: '<svg id="i-arrow-right" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="32" height="32" fill="none" stroke="currentcolor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"> <path d="M22 6 L30 16 22 26 M30 16 L2 16" /> </svg>'
+        //   }
+        // ]
+      },
+    };
+  },
+  created() {
+    console.log(window.location.pathname);
+    this.currentPath = window.location.pathname;
+  },
+  computed: {
+    // currentPath(){
+    //   return window.location.pathname;
+    // }
+  },
+  methods: {
+    menuClick($event) {
+      window.history.pushState(
+        "object or string",
+        "Title",
+        `${$event.replaceAll(" ", "-").toLowerCase()}`
+      );
+      this.currentPath = window.location.pathname;
+      console.log($event, window.location.pathname);
+    },
+  },
+};
 </script>
 
 <style>
@@ -21,8 +173,15 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+.menu-bar {
+  margin-left: 200px;
+  margin-right: 200px;
+}
+.body-item {
+  margin-left: 240px;
+  margin-right: 240px;
+  height: 92vh;
 }
 </style>
---
+++ src/components/MdRender.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <object
+      data="./assets/javascript-entry-level.md"
+      width="300"
+      height="200"
+    ></object>
+    <vue-markdown>
+      {{ text }}
+    </vue-markdown>
+  </div>
+</template>
+<script>
+import VueMarkdown from "vue-markdown";
+export default {
+  components: {
+    VueMarkdown,
+  },
+  created() {},
+  data() {
+    return {
+      text: `
+## Code
+Indented code
+
+    // Some comments
+    line 1 of code
+    line 2 of code
+    line 3 of code
+
+
+Block code "fences"
+
+## Tables
+
+| Option | Description |
+| ------ | ----------- |
+| data   | path to data files to supply the data that will be passed into templates. |
+| engine | engine to be used for processing templates. Handlebars is the default. |
+| ext    | extension to be used for dest files. |
+| 测试    | extension to be used for dest files. |
+`,
+    };
+  },
+};
+</script>
